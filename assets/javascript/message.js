08e635499e6fd150b88bc261dac3eb8a1aa09e8f
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-14 12:34:41 +0300

    add url directly inside the fetch

--- assets/javascript/message.js
+++ assets/javascript/message.js
@@ -1,12 +1,21 @@
-const api = 'http://localhost/portfolio-design-frontend/api/'
-
 async function getData() {
-  const response = await fetch(api + 'get_messages_api.php')
+  const response = await fetch(
+    'http://localhost/portfolio-design-frontend/api/get_messages_api.php'
+  )
   const data = await response.json()
 
-  let div = document.querySelector('#test')
+  let table_body = document.querySelector('.table-body')
+  let content_body = ''
 
-  div.innerHTML = data
+  data.forEach((row) => {
+    content_body += '<tr>'
+    content_body += '<td>' + row.full_name + '</td>'
+    content_body += '<td>' + row.email + '</td>'
+    content_body += '<td>' + row.phone + '</td>'
+    content_body += '<td>' + row.message + '</td>'
+    content_body += '</tr>'
+  })
+  table_body.innerHTML = content_body
 }
 
 getData()
