4fe68184a1deec16e9c09dafb7ca5daa1e63d1c8
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-14 13:11:25 +0300

    adjust the parts of the code into functions

--- assets/javascript/connect.js
+++ assets/javascript/connect.js
@@ -1,36 +1,3 @@
-// const form = document.querySelector('#form')
-
-// const full_name = document.querySelector('#full_name')
-// const email = document.querySelector('#email')
-// const phone = document.querySelector('#phone')
-// const message = document.querySelector('#message')
-
-// const form_data = (data) => {
-//   let formData = new FormData()
-//   data.forEach((col) => {})
-// }
-const save_message = async () => {
-  try {
-    const res = await fetch(
-      'http://localhost/portfolio-design-frontend/api/save_message_api.php',
-      {
-        method: 'POST',
-        // cache: 'no-cache',
-        // cors: 'no-cors',
-        // credentials: 'same-origin',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      }
-    )
-    // const content = await res.json()
-    return res
-  } catch (error) {
-    console.log('error', error.response)
-  }
-}
-
 const api = 'http://localhost/portfolio-design-frontend/api/'
 
 form.addEventListener('submit', (e) => {
@@ -45,18 +12,7 @@ form.addEventListener('submit', (e) => {
   let error = checkValidation(name_val, email_val, phone_val, message_val)
 
   if (!isEmpty(error)) {
-    if (new_div.classList.contains('success')) {
-      new_div.classList.remove('success')
-    }
-    if (!new_div.classList.contains('error')) {
-      new_div.classList.add('error')
-    }
-    new_div.textContent = error
-    new_div.style.display = 'block'
-
-    setTimeout(() => {
-      new_div.style.display = 'none'
-    }, 5000)
+    displayErrorSuccess(new_div, error, 'success', 'error')
     return
   } else {
     console.log(api + 'save_message_api.php')
@@ -68,32 +24,47 @@ form.addEventListener('submit', (e) => {
         phone: phone_val,
         message: message_val,
       }),
-      // headers: {
-      //   'Content-type': 'application/json; charset=UTF-8',
-      // },
     })
       .then((response) => response.json())
       .then((data) => {
         console.log(data.success)
         if (data.success) {
-          new_div.classList.remove('error')
-          new_div.classList.add('success')
-          new_div.textContent = 'Message sent successfully'
-          new_div.style.display = 'block'
-          // close the div in 5 secs
-          setTimeout(() => {
-            new_div.style.display = 'none'
-          }, 5000)
+          displayErrorSuccess(
+            new_div,
+            'Message sent successfully',
+            'error',
+            'success'
+          )
+
+          full_name.value = ''
+          email.value = ''
+          phone.value = ''
+          message.value = ''
         } else {
-          new_div.classList.remove('success')
-          new_div.classList.add('error')
-          new_div.textContent = 'Something went wrong, try again'
-          new_div.style.display = 'block'
-          setTimeout(() => {
-            new_div.style.display = 'none'
-          }, 5000)
+          displayErrorSuccess(
+            new_div,
+            'Something went wrong, try again',
+            'success',
+            'error'
+          )
         }
       })
       .catch((error) => console.error('Error:', error))
   }
 })
+
+//function to diplay error or success messages -- messages will disappear after 10 sec
+const displayErrorSuccess = (element, text, classToremove, classToAdd) => {
+  if (new_div.classList.contains(classToremove)) {
+    new_div.classList.remove(classToremove)
+  }
+  if (!new_div.classList.contains(classToAdd)) {
+    new_div.classList.add(classToAdd)
+  }
+  element.textContent = text
+  element.style.display = 'block'
+
+  setTimeout(() => {
+    element.style.display = 'none'
+  }, 5000)
+}
