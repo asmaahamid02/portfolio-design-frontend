65472ad378d3a9915756a202846295e2fa692eec
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-14 03:23:39 +0300

    add isset() method to save_message_api.php file

--- api/save_message_api.php
+++ api/save_message_api.php
@@ -1,20 +1,26 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
 include_once "connection.php";
 
-$full_name = $_POST['fullName'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+if (isset($_POST['fullName']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
+    $full_name = $_POST['fullName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
 
-$sql = "INSERT INTO messages (full_name, email, phone, message) VALUES (?,?,?,?)";
+    $sql = "INSERT INTO messages (full_name, email, phone, message) VALUES (?,?,?,?)";
 
-$statement = $connection->prepare($sql);
-$statement->bind_param('ssss', $full_name, $email, $phone, $message);
-$statement->execute();
+    $statement = $connection->prepare($sql);
+    $statement->bind_param('ssss', $full_name, $email, $phone, $message);
+    $statement->execute();
 
-$response['status'] = 'success';
+    $response['status'] = 'success';
 
-echo json_encode($response);
+    echo json_encode($response);
 
-$statement->close();
-$connection->close();
+    $statement->close();
+    $connection->close();
+}
