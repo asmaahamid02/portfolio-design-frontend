5d38db4fa7634fac4fd591df7c39fcecfdb552dd
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-14 10:06:17 +0300

    edit variables names in save_message_api.php file

--- api/save_message_api.php
+++ api/save_message_api.php
@@ -5,8 +5,8 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorizatio
 
 include_once "connection.php";
 
-if (isset($_POST['fullName']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
-    $full_name = $_POST['fullName'];
+if (isset($_POST['full_name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['message'])) {
+    $full_name = $_POST['full_name'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
@@ -17,7 +17,7 @@ if (isset($_POST['fullName']) && isset($_POST['email']) && isset($_POST['phone']
     $statement->bind_param('ssss', $full_name, $email, $phone, $message);
     $statement->execute();
 
-    $response['status'] = 'success';
+    $response['success'] = true;
 
     echo json_encode($response);
 
